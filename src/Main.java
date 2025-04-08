dd615e228f28a3873a652e750877623fe440fd80
Gestringc <chris.gestring@students.kennesaw.edu>
2025-04-07 21:59:55 -0500

    OH YEAH

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class Main extends Application {
+    private static final String APPLICATION_TITLE = "Edit Customer Accounts";
+    private static final double SCENE_WIDTH = 850;
+    private static final double SCENE_HEIGHT = 650;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = loadFXMLLayout();
+        configurePrimaryStage(primaryStage, root);
+    }
+
+    private Parent loadFXMLLayout() throws Exception {
+        try {
+            URL fxmlUrl = getClass().getResource("/Resources/MainView.fxml");
+            System.out.println("Resolved URL: " + fxmlUrl);
+
+            FXMLLoader loader = new FXMLLoader(fxmlUrl);
+            return loader.load();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    private void configurePrimaryStage(Stage stage, Parent root) {
+        stage.setTitle(APPLICATION_TITLE);
+        stage.setScene(new Scene(root, SCENE_WIDTH, SCENE_HEIGHT));
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Github");
-        System.out.println("Example");
-    }}
+        System.out.println("Path: " + Main.class.getResource("/Resources/CustomerAccountTable.fxml"));
+        launch(args);
+    }
+}
